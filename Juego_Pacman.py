b1b1899f12fb16973d6e5431d1f4e87fb23f0bb3
ErnestoIRS <A00827434@itesm.mx>
2020-10-28 16:21:19 -0600

    Comentarios iniciales

--- Juego_Pacman.py
+++ Juego_Pacman.py
@@ -2,17 +2,30 @@ from random import choice
 from turtle import *
 from freegames import floor, vector
 
+#Variable que indica el puntaje.
 state = {'score': 0}
+
+#puntero que indica el camino que sigue pacman
 path = Turtle(visible=False)
+
+#puntero que indica en donde se va a mostrar el puntaje
 writer = Turtle(visible=False)
+
+#Vector que indica la dirección de movimiento
 aim = vector(5, 0)
+
+#vector que indica la posición inicial de pac-man
 pacman = vector(-40, -80)
+
+#vectores que indican la posición inicial de los fantasmas
 ghosts = [
     [vector(-180, 160), vector(5, 0)],
     [vector(-180, -160), vector(0, 5)],
     [vector(100, 160), vector(0, -5)],
     [vector(100, -160), vector(-5, 0)],
 ]
+
+#Matriz que indica la forma del laberinto. 0=no hay camino, 1=camino
 tiles = [
     0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
     0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0,
@@ -36,6 +49,7 @@ tiles = [
     0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
 ]
 
+#Función que se encarga de dibujar los recuadros que indican el camino
 def square(x, y):
     "Draw square using path at (x, y)."
     path.up()
@@ -43,50 +57,61 @@ def square(x, y):
     path.down()
     path.begin_fill()
 
+    #Se dibuja un recuadro con las dimensiones dadas
     for count in range(4):
         path.forward(20)
         path.left(90)
 
     path.end_fill()
 
+#Función que regresa la posición de cierto punto en el mapa
 def offset(point):
     "Return offset of point in tiles."
+    
+    #Escala las posiciones en x y en y a las medidas de cada cuadro del mapa.
     x = (floor(point.x, 20) + 200) / 20
     y = (180 - floor(point.y, 20)) / 20
     index = int(x + y * 20)
     return index
 
+#funcion que valida si el punto indicado por la funcion offset es parte del camino o no
 def valid(point):
     "Return True if point is valid in tiles."
     index = offset(point)
-
+    
+    #Si las coordenadas del punto son un 0 en tiles, regresa false.
     if tiles[index] == 0:
         return False
-
+    
+    #Cambia las coordenadas de index para evaluar la posición de la "esquina" contraria, simulando un cuadrado
     index = offset(point + 19)
 
     if tiles[index] == 0:
         return False
 
+    #En caso de no cumplir las condiciones anteriores, regresa la posición dada inicialmente (true)
     return point.x % 20 == 0 or point.y % 20 == 0
 
 def world():
     "Draw world using path."
+    #indica el color del camino y de las barreras
     bgcolor('black')
     path.color('blue')
-
+    
+    #Evalua cuadro por cuadro en "tiles"
     for index in range(len(tiles)):
         tile = tiles[index]
-
-        if tile > 0:
+        
+        #Si el cuadro es 1 en tiles, se dibuja un cuadrado
+        if tile == 1:
             x = (index % 20) * 20 - 200
             y = 180 - (index // 20) * 20
             square(x, y)
-
-            if tile == 1:
-                path.up()
-                path.goto(x + 10, y + 10)
-                path.dot(2, 'white')
+            path.up()
+            path.goto(x + 10, y + 10)
+            path.dot(2, 'white')
+                     
+            
 
 def move():
     "Move pacman and all ghosts."
